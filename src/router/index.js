f54c572c4d14b093429ace5bdfe6b1fc3a516d50
zhangpeng <762688256@qq.com>
2021-05-02 19:32:04 +0800

    推荐页面开发

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: () => import('@/views/recommend')
+  },
+  {
+    path: '/singer',
+    component: () => import('@/views/singer')
+  },
+  {
+    path: '/top-list',
+    component: () => import('@/views/top-list')
+  },
+  {
+    path: '/search',
+    component: () => import('@/views/search')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const registerRouter = require('./backend/router')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  devServer: {
+    before(app) {
+      registerRouter(app)
+    }
+  }
+}
